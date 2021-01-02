cf1206687a3d239709b9cb195360ebf4821141fb
Zepu Zhang <zepu.zhang@gmail.com>
2021-01-02 13:48:53 -0800

    build and test

--- build_docker.sh
+++ build_docker.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-MINI=$(bash -c "$(docker run --rm zppz/tiny:21.01.02 cat /usr/tools/find-image)" -- zppz/mini)
-cmd="$(docker run --rm ${MINI} cat /usr/tools/build-docker)"
-bash -c "${cmd}" -- $@
+TINY=zppz/tiny:21.01.02
+PARENT=$(bash -c "$(docker run --rm ${TINY} cat /usr/tools/find-image)" -- zppz/py3)
+TAG=$(docker run --rm ${TINY} make-date-version)
+MINI=$(bash -c "$(docker run --rm ${TINY} cat /usr/tools/find-image)" -- zppz/mini)
+cmd="$(docker run --rm ${MINI} cat /usr/tools/build-docker-py)"
+
+bash -c "${cmd}" -- --build-arg PARENT=${PARENT} --tag ${TAG}
+
